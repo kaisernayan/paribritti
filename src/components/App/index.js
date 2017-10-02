b4b0901dc20de85a304836672adac09bd0b2de4c
Kawsar Hamid <7290933+kaisernayan@users.noreply.github.com>
2017-10-02 07:12:08 +0600

    Initial Project
    Initial Project

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../Header/'
+import Footer from '../Footer/'
+import VisibleTodoList from '../containers/VisibleTodoList'
+import AddTodo from '../containers/AddTodo'
+
+const App = () => (
+  <div>
+    <Header />
+  </div>
+)
+
+export default App
---
+++ src/components/Header/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Header = () => (
+    <div className="nav-wrapper">
+        <nav className="container navbar navbar-expand-md navbar-white fixed-top bg-white">
+            <a className="navbar-brand" href="#"><span>Paribri</span>tti Online</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarCollapse">
+                <ul className="navbar-nav mr-auto float-right">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link disabled" href="#">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+)
+
+
+export default Header
